a52ef5e11d8e31ce5f9f3af657b2b8098537adeb
admin <admin@tbstaxi.ru>
2019-05-26 19:10:23 +0300

    Add setting in init.rb

--- init.rb
+++ init.rb
@@ -2,13 +2,32 @@ Redmine::Plugin.register :redmine_tbstaxi do
   name 'Redmine Tbstaxi plugin'
   author 'lek'
   description 'Patches for tbstaxi redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://tbstaxi.ru'
   author_url 'https://tbstaxi.ru'
 
   requires_redmine version_or_higher: '3.0.0'
 
+  settings :default => {
+    'tbs_settings_parent_and_child_issues' => false,
+    'tbs_settings_actionmailer_log' => false,
+    'tbs_settings_mailer_log' => false,
+    'tbs_settings_l4_log' => false,
+    'tbs_settings_issue_open_by_email' => false,
+    'tbs_settings_add_resent_from' => false,
+    'tbs_settings_add_anonymous_from' => false,
+    'tbs_settings_message_id_re' => false,
+    'tbs_settings_changeauthor' => false,
+    'tbs_settings_verify_ssl_disable' => false
+    }, :partial => 'settings/ts_settings'
+
+
   Rails.configuration.to_prepare do
+    if Setting['plugin_redmine_tbstaxi']['tbs_settings_parent_and_child_issues']
+      require 'issue_patch'
+
+    end
+
     require 'mail_handler_patch'
     MailHandler.send :include, MailHandlerPatch
 
